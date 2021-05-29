65e34f8b0ba2a82a043119485f0141370b068b30
Taylor Cella <taylor@sitequesttech.com>
2021-05-29 12:24:19 -0500

    WIP table, get data into single array

--- mailshake-exercise/src/app/star-wars-people/star-wars-people.component.ts
+++ mailshake-exercise/src/app/star-wars-people/star-wars-people.component.ts
@@ -1,14 +1,22 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map, takeUntil } from 'rxjs/operators';
+import { map, takeUntil, pluck } from 'rxjs/operators';
 import { Observable, Subject } from 'rxjs';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
 
 export interface peopleModel {
   name: string;
   world: string;
-  dob: Date;
-  film: string
+  dob: string;
+  films: string[];
+}
+
+export interface serverResponse {
+  count: number;
+  next: string;
+  previous?: string;
+  results: [{}];
 }
 
 @Component({
@@ -19,14 +27,33 @@ export interface peopleModel {
 export class StarWarsPeopleComponent implements OnInit, OnDestroy {
   public dataSource = new MatTableDataSource();
   private unsubscribe: Subject<void> = new Subject();
+  displayedColumns: string[] = ['name', 'homeWorld', 'birthYear', 'films'];
+  public test: peopleModel[] = [];
+
+  @ViewChild(MatSort) sort: MatSort;
+
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.http.get<[]>('https://swapi.dev/api/people/').pipe(
-      map(response => {
-        console.log(response);
-        this.dataSource.data = response;
-        console.log(this.dataSource.data);
+    this.getData('https://swapi.dev/api/people/');
+  }
+
+  // ngAfterViewInit() {
+  //   this.dataSource.sort = this.sort;
+  // }
+
+  getData(url?: string) {
+    this.http.get<serverResponse>(url).pipe(
+      map(firstResponse => {
+        firstResponse.results.forEach(res => {
+          this.test.push({name: res['name'], world: res['homeworld'], dob: res['birth_year'], films: res['films']})
+        });
+        if(firstResponse.next != null) {
+          this.getData(firstResponse.next);
+        }
+        else {
+          this.dataSource.data = this.test;
+        }
       }),
       takeUntil(this.unsubscribe)
     ).subscribe();
